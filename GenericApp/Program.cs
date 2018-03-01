88c7310043d814d707f6b0df6a7afc6e98273d2c
rahul <rahul@pawar.com.au>
2018-03-01 21:26:21 +1100

    Test commit

--- GenericApp/Program.cs
+++ GenericApp/Program.cs
@@ -14,7 +14,7 @@ namespace GenericApp
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run(); // some comment.
+            BuildWebHost(args).Run(); // some comment. some more.
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
